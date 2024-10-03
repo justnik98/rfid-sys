b1ff4680a16c8e92d7990ac8eee6fd8d5cf2e14e
justnik98 <yannik98@yandex.ru>
2024-10-03 20:02:12 +0300

    теперь генерирует только для валидных значений L = 2**q

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import pandas as pd
 import numpy as np
@@ -21,20 +22,34 @@ def round_gen(L: int, N: int):
     return s, e, c
 
 
-def generate_data(num: int, N_max: int, L_max: int):
+def generate_data(num, N_max: int, q_max: int):
+    start = time.time()
     data_x = []
     data_y = []
-    for i in range(0, num):
-        for L in range(1, L_max + 1):
-            for N in range(1, N_max + 1):
+    count = 0
+    q = 0
+    while q != q_max:
+        L = 2 ** q
+        for N in range(1, N_max + 1):
+            for i in range(0, num):
                 s, e, c = round_gen(L, N)
                 x = [s, e, c, L]
                 data_x.append(x)
                 data_y.append(N)
+                count += 1
+        q += 1
+
+        now = time.time()
+        percent = q * 100 / q_max
+        time_sp = round(now - start, 3)
+        remained = time_sp / q * q_max - time_sp
+        print(
+            f'total count {count} = {round(percent, 3)} %, elapsed time = {time_sp} sec, remained = {round(remained, 3)} sec')
     df_x = pd.DataFrame(data_x)
     df_y = pd.DataFrame(data_y)
-    df_x.to_csv('./data_x', header=False, index=False)
-    df_y.to_csv('./data_y', header=False, index=False)
+    df_x.to_csv('./data_x.csv', header=False, index=False)
+    df_y.to_csv('./data_y.csv', header=False, index=False)
+    return data_x, data_y
 
 
-generate_data(1000, 10, 1024)
+generate_data(1000, 256, 8)
